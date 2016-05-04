d6a43a4ee1700bd46e050dd99ee66996f7423abf
Yung Kim <gim.yoong@gmail.com>
2016-05-04 10:24:18 -0700

    updated 05_ninja_gold (fixed initial money issue, added activities output with history log)

--- 0503 python/05_ninja_gold/server.py
+++ 0503 python/05_ninja_gold/server.py
@@ -4,8 +4,7 @@ import random, datetime
 app = Flask(__name__)
 app.secret_key = 'ThisIsSecret' # you need to set a secret key for security purposes
 
-def total(x):
-  session['total'] = 0;
+def total(x): # set global session total amount of money here
   try:
     session['total'] += x
   except KeyError:
@@ -13,33 +12,40 @@ def total(x):
 
 @app.route('/')     
 def index():
+  session['total'] = 0; # initial money must be set here, loads one time until clearsession() is called
+  session['sResult'] = ""
   return render_template('index.html')
 
 @app.route('/process_money', methods=['POST'])
 def process():
   print "Got Post Info"
-  # session['total'] = 100;
   money = request.form['building']
+
   if (money == 'farm'):
     plus = random.randrange(10, 21)
     session['total'] += plus
     result = "Earned "+str(plus)+" golds from the farm! "+str(datetime.datetime.now())
+    session['sResult'] = str(result) + "\n" + session['sResult']  
   elif (money == 'cave'):
     plus = random.randrange(5, 11)
     session['total'] += plus
-    result = "Earned "+str(plus)+" golds from the farm! "+str(datetime.datetime.now())
+    result = "Earned "+str(plus)+" golds from the cave! "+str(datetime.datetime.now())
+    session['sResult'] = str(result) + "\n" + session['sResult']
   elif (money == 'house'):
     plus = random.randrange(2, 6)
     session['total'] += plus
-    result = "Earned "+str(plus)+" golds from the farm! "+str(datetime.datetime.now())
+    result = "Earned "+str(plus)+" golds from the house! "+str(datetime.datetime.now())
+    session['sResult'] = str(result) + "\n" + session['sResult']
   elif (money == 'casino'):
     plus = random.randrange(-50, 51)
     session['total'] += plus
     if (plus > 0):
-      result = "Earned "+str(plus)+" golds from the farm! "+str(datetime.datetime.now())
+      result = "Earned "+str(plus)+" golds from the Casino! "+str(datetime.datetime.now())
+      session['sResult'] = str(result) + "\n" + session['sResult']
     else:
       result = "Entered a casino and lost "+str(plus)+" golds... Ouch..  "+str(datetime.datetime.now())
-  return render_template('/index.html', money=money, total=session['total'], result=result)
+      session['sResult'] = str(result) + "\n" + session['sResult']
+  return render_template('/index.html', money=money, total=session['total'], result=result, sResult=session['sResult'])
 
 @app.route('/clear')
 def clearsession():
