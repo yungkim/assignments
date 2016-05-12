adcc0065a8003b23f1adb44fa20df59c1fcf22b5
Yung Kim <gim.yoong@gmail.com>
2016-05-11 18:03:36 -0700

    added python/python_mvc/01_time_display

---
+++ python/pylot_mvc/01_time_display/controllers/Times.py
@@ -0,0 +1,45 @@
+from time import strftime
+"""
+    Sample Controller File
+
+    A Controller should be in charge of responding to a request.
+    Load models to interact with the database and load views to render them to the client.
+
+    Create a controller using this template
+"""
+from system.core.controller import *
+
+class Times(Controller):
+    def __init__(self, action):
+        super(Times, self).__init__(action)
+        """
+            This is an example of loading a model.
+            Every controller has access to the load_model method.
+        """
+        self.load_model('WelcomeModel')
+        self.db = self._app.db
+
+        """
+        
+        This is an example of a controller method that will load a view for the client 
+
+        """
+        
+    def index(self):
+        """
+        A loaded model is accessible through the models attribute 
+        self.models['WelcomeModel'].get_users()
+        
+        self.models['WelcomeModel'].add_message()
+        # messages = self.models['WelcomeModel'].grab_messages()
+        # user = self.models['WelcomeModel'].get_user()
+        # to pass information on to a view it's the same as it was with Flask
+        
+        # return self.load_view('index.html', messages=messages, user=user)
+        """
+        time1 = strftime("%b %d, %Y")
+        time2 = strftime("%I:%M %p")
+        return self.load_view('index.html', time1=time1, time2=time2)
+
+
+
