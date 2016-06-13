f15241b8ed1e0c2dd3e4e80dd7cd1bd2c6098457
Yung Kim <gim.yoong@gmail.com>
2016-06-13 13:06:32 -0700

    added ruby/rails_ii/01_users

---
+++ ruby/rails_ii/01_user/app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+  has_secure_password
+  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+  validates :name, :email, :password, presence: true;
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
+end
