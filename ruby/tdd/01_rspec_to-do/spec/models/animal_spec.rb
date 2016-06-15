c0b97cd224057f2720785696a082f5025dcfe6d6
Yung Kim <gim.yoong@gmail.com>
2016-06-15 16:57:38 -0700

    added ruby/tdd/01_rspec_to-do

---
+++ ruby/tdd/01_rspec_to-do/spec/models/animal_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Animal, type: :model do
+  pending "it is a valid person's name"
+  pending "it is a valid pet's name"
+  pending "it is a valid age for a person"
+  pending "it is a valid age for a pet"
+end
