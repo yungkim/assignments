add4507fac2cd2398b48867e49a450621d4377b4
Yung Kim <gim.yoong@gmail.com>
2016-08-11 10:23:43 -0700

    updated mean/full_mean/01_file_structure_simple_server (filter by date on index.html, currentValue variable on edit.html)

--- mean/full_mean/01_file_structure_simple_server/server/controllers/editController.js
+++ mean/full_mean/01_file_structure_simple_server/server/controllers/editController.js
@@ -1,12 +1,15 @@
 /* EDIT CONTROLLER: this controller uses 'this', and the controlValue seems to not update (a bug for you to fix! possibly one new line of code ~ 14 characters, and one modification of something that already exists)*/
     app.controller('editController', ['$scope', 'userFactory', '$location', '$routeParams', function($scope, userFactory, $location, rParams) {
       /* Public Properties */
+      var self = this;
       this.controlValue = "Current Name:";
 
       /* Public Methods */
       this.getUser = function() {
         userFactory.show(rParams.id, function passedToUserFactoryShow(user) {
           $scope.user = user;
+          //console.log(user) // one user
+
         })
       }
 
@@ -15,7 +18,7 @@
           $scope.user = userFromFactory;
           // what is this?
 
-          // $scope.$parent.controlValue = "Updated Name: "
+          self.controlValue = "Updated Name: "
 
           // $location.url('/edit/rParams.id')
         });
